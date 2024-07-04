5da0dc1e34301b6dfa8fe6303e37f2f7e2a8d264
Shaashvat Mittal <shaashvatmittal2004@gmail.com>
2024-07-04 23:05:50 +0530

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function openWindow() {
     var newWindow = window.open('', '_blank');
-    newWindow.document.write("<html><head><title>Coming Soon</title></head><body><h1>Coming Soon</h1></body></html>");
+    newWindow.document.write("<html><head><title>Connect 4</title></head><body><h1>Coming Soon</h1></body></html>");
     newWindow.focus();
   }
